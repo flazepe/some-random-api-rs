87887800d1a59ee62271d8db9c2212b644883e14
flazepe <flazepe@gmail.com>
2023-05-30 01:31:13 +0800

    feat: "document" more endpoints

--- src/endpoints/canvas/overlay.rs
+++ src/endpoints/canvas/overlay.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 pub struct CanvasOverlayEndpoint(pub(crate) Requester);
 
 impl CanvasOverlayEndpoint {
+    /// Yes
     pub async fn comrade<T: ToString>(&self, avatar_url: T) -> Result<Vec<u8>> {
         self.0
             .request_image(
@@ -13,12 +14,14 @@ impl CanvasOverlayEndpoint {
             .await
     }
 
+    /// Give your avatar a rainbow overlay
     pub async fn gay<T: ToString>(&self, avatar_url: T) -> Result<Vec<u8>> {
         self.0
             .request_image("canvas/overlay/gay", &[("avatar", avatar_url.to_string())])
             .await
     }
 
+    /// Give your avatar a glass effect overlay
     pub async fn glass<T: ToString>(&self, avatar_url: T) -> Result<Vec<u8>> {
         self.0
             .request_image(
@@ -28,12 +31,14 @@ impl CanvasOverlayEndpoint {
             .await
     }
 
+    /// Go to horny jail
     pub async fn jail<T: ToString>(&self, avatar_url: T) -> Result<Vec<u8>> {
         self.0
             .request_image("canvas/overlay/jail", &[("avatar", avatar_url.to_string())])
             .await
     }
 
+    /// Mission passed
     pub async fn passed<T: ToString>(&self, avatar_url: T) -> Result<Vec<u8>> {
         self.0
             .request_image(
@@ -43,6 +48,7 @@ impl CanvasOverlayEndpoint {
             .await
     }
 
+    /// I will not enter a description so you will get triggered
     pub async fn triggered<T: ToString>(&self, avatar_url: T) -> Result<Vec<u8>> {
         self.0
             .request_image(
@@ -52,6 +58,7 @@ impl CanvasOverlayEndpoint {
             .await
     }
 
+    /// Wastedeeznuts
     pub async fn wasted<T: ToString>(&self, avatar_url: T) -> Result<Vec<u8>> {
         self.0
             .request_image(
--- src/endpoints/image.rs
+++ src/endpoints/image.rs
@@ -4,46 +4,57 @@ use anyhow::Result;
 pub struct ImageEndpoint(pub(crate) Requester);
 
 impl ImageEndpoint {
+    /// An endpoint that sends a random image of a bird
     pub async fn bird(&self) -> Result<Image> {
         self.0.request("img/bird").await
     }
 
+    /// An endpoint that sends a random image of a cat
     pub async fn cat(&self) -> Result<Image> {
         self.0.request("img/cat").await
     }
 
+    /// An endpoint that sends a random image of a dog
     pub async fn dog(&self) -> Result<Image> {
         self.0.request("img/dog").await
     }
 
+    /// An endpoint that sends a random image of a fox
     pub async fn fox(&self) -> Result<Image> {
         self.0.request("img/fox").await
     }
 
+    /// An endpoint that sends a random image of a kangaroo
     pub async fn kangaroo(&self) -> Result<Image> {
         self.0.request("img/fox").await
     }
 
+    /// An endpoint that sends a random image of a koala
     pub async fn koala(&self) -> Result<Image> {
         self.0.request("img/koala").await
     }
 
+    /// An endpoint that sends a random image of a panda
     pub async fn panda(&self) -> Result<Image> {
         self.0.request("img/panda").await
     }
 
+    /// An endpoint that sends a random image of a pikachu
     pub async fn pikachu(&self) -> Result<Image> {
         self.0.request("img/pikachu").await
     }
 
+    /// An endpoint that sends a random image of a raccoon
     pub async fn raccoon(&self) -> Result<Image> {
         self.0.request("img/raccoon").await
     }
 
+    /// An endpoint that sends a random image of a red panda
     pub async fn red_panda(&self) -> Result<Image> {
         self.0.request("img/red_panda").await
     }
 
+    /// An endpoint that sends a random image of a whale
     pub async fn whale(&self) -> Result<Image> {
         self.0.request("img/whale").await
     }
